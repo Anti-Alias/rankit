f7318ab5ba2943a47d70d9db88dbc73f7d394cb7
William Cahill <drewlikesramen@gmail.com>
2023-11-02 15:06:44 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use dotenvy::dotenv;
-use rankit::{AppResources, read_var, env};
+use rankit::AppResources;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     dotenv()?;
     env_logger::init();
     let resources = AppResources::from_env().await?;
-    
     Ok(())
 }
