148109f40bb5551c5c5af3631cc17215d2c9ba46
William Cahill <drewlikesramen@gmail.com>
2023-11-02 18:29:42 -0400

    Got account creation working

--- src/account.rs
+++ src/account.rs
@@ -1,4 +1,54 @@
-use sqlx::PgPool;
+use axum::{Json, extract::State};
+use sqlx::{PgPool, FromRow};
+use serde::{Deserialize, Serialize};
+use tokio::try_join;
+use crate::{JsonResponse, AppState, AppError};
+
+/// Request to create an account.
+#[derive(Deserialize, Debug)]
+pub struct CreateAccountRequest {
+    pub name: String,
+    pub email: String,
+    pub password: String
+}
+
+/// Response for a [`CreateAccountRequest`].
+#[derive(Serialize, FromRow)]
+pub struct CreateAccountResponse {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+}
+
+pub async fn create_account(
+    State(state): State<AppState>,
+    Json(request): Json<CreateAccountRequest>
+) -> JsonResponse<CreateAccountResponse> {
+    
+    // Checks for duplicates
+    let name_count = sqlx::query_as::<_, (i64,)>("SELECT COUNT(*) FROM account WHERE name = $1")
+        .bind(&request.name)
+        .fetch_one(&state.pool);
+    let email_count = sqlx::query_as::<_, (i64,)>("SELECT COUNT(*) FROM account WHERE email = $1")
+        .bind(&request.email)
+        .fetch_one(&state.pool);
+    let (name_count, email_count) = try_join!(name_count, email_count)?;
+    if name_count.0 != 0 {
+        return Err(AppError::DuplicateAccountName);
+    }
+    if email_count.0 != 0 {
+        return Err(AppError::DuplicateAccountEmail);
+    }
+
+    // Fetches data
+    let response: CreateAccountResponse = sqlx::query_as("INSERT INTO account (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email")
+        .bind(request.name)
+        .bind(request.email)
+        .bind(request.password)
+        .fetch_one(&state.pool)
+        .await?;
+    Ok(Json(response))
+}
 
 /// Creates a root user if it does not already exist.
 pub async fn create_root_account(
@@ -9,9 +59,7 @@ pub async fn create_root_account(
 ) -> Result<(), anyhow::Error> {
 
     // Quits if root account already exists
-    let row: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM account WHERE role='root'")
-        .fetch_one(pool)
-        .await?;
+    let row: (i64,) = sqlx::query_as("SELECT COUNT(*) FROM account WHERE role='root'").fetch_one(pool).await?;
     let root_account_exists = row.0 != 0;
     if root_account_exists {
         return Ok(());
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ pub mod env;
 pub mod account;
 pub mod migrate;
 
-use axum::routing::{Router, get};
+use axum::{routing::{Router, get, post}, response::{IntoResponse, Response}, http::StatusCode, Json};
 use derive_more::{Error, Display, From};
 use sqlx::{postgres::PgPoolOptions, PgPool};
+
+pub type JsonResponse<T> = Result<Json<T>, AppError>;
 
 /// Shared resources used by application.
 #[derive(Clone)]
@@ -13,7 +15,7 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub async fn from_env() -> Result<Self, AppError> {
+    pub async fn from_env() -> Result<Self, StartupError> {
         log::info!("Connecting to DB");
         let pg_str = read_var(env::APP_DB)?;
         let pool = PgPoolOptions::new()
@@ -24,17 +26,45 @@ impl AppState {
     }
 }
 
+/// Error that can occur when creating an [`AppState`].
 #[derive(Error, Display, Debug, From)]
-pub enum AppError {
+pub enum StartupError {
     DotenvyError(dotenvy::Error),
     #[from(ignore)]
     MissingEnvironmentVar(#[error(not(source))] String),
     SqlxError(sqlx::Error)
 }
 
-pub fn read_var(var_name: &str) -> Result<String, AppError> {
+
+/// Error that can occur when the application is running.
+#[derive(Error, Display, Debug, From)]
+pub enum AppError {
+    DuplicateAccountName,
+    DuplicateAccountEmail,
+    SqlxError(sqlx::Error),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        match self {
+            AppError::DuplicateAccountName => (StatusCode::BAD_REQUEST, "Duplicate account name"),
+            AppError::DuplicateAccountEmail => (StatusCode::BAD_REQUEST, "Duplicate account email"),
+            AppError::SqlxError(error) => {
+                if is_unique_violation(&error) {
+                    (StatusCode::BAD_REQUEST, "Duplicate data")
+                }
+                else {
+                    log::error!("SQL Error: {error}");
+                    (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+                }
+            },
+        }.into_response()
+    }
+}
+
+pub fn read_var(var_name: &str) -> Result<String, StartupError> {
     let Ok(value) = dotenvy::var(var_name) else {
-        return Err(AppError::MissingEnvironmentVar(var_name.into()));
+        return Err(StartupError::MissingEnvironmentVar(var_name.into()));
     };
     Ok(value)
 }
@@ -43,10 +73,23 @@ pub fn read_var(var_name: &str) -> Result<String, AppError> {
 pub async fn app(state: AppState) -> Result<Router, anyhow::Error> {
     let router = Router::new()
         .route("/health", get(health))
+        .route("/account", post(account::create_account))
         .with_state(state);
     Ok(router)
 }
 
  async fn health() -> &'static str {
     "Server is healthy"
+ }
+
+ pub fn is_unique_violation(error: &sqlx::Error, ) -> bool {
+    match error {
+        sqlx::Error::Database(err) => {
+            match err.kind() {
+                sqlx::error::ErrorKind::UniqueViolation => true,
+                _ => false,
+            }
+        },
+        _ => false,
+    }
  }
