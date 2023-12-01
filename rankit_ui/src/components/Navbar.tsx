ce484035025c5db09c2f03cc3b8eef43074e5c27
William Cahill <drewlikesramen@gmail.com>
2023-12-01 14:31:01 -0500

    Refactored to use basic routing

--- rankit_ui/src/components/Navbar.tsx
+++ rankit_ui/src/components/Navbar.tsx
@@ -1,27 +1,22 @@
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
     <div className={styles.Navbar}>
         <div className={styles.inner}>
-
-            {/* Title */}
             <div className={styles.left}>
-                <h2>Thingelo</h2>
+                <Link to='/' className={styles.site}><h2>Thingelo</h2></Link>
             </div>
-
-            {/* Option list */}
             <div className={styles.center}>
-                <div className={styles.option}>About</div>
-                <div className={styles.option}>Things</div>
-                <div className={styles.option}>Categories</div>
+                <Link to='/'            className={styles.option}>About</Link>
+                <Link to='/things'      className={styles.option}>Things</Link>
+                <Link to='/categories'  className={styles.option}>Categories</Link>
+            </div>
+            <div className={styles.right}>
+                <Link to='/login'   className={styles.login}>Log in</Link> |
+                <Link to='/signup'  className={styles.signup}>Sign Up</Link>
             </div>
             
-            {/* Login / Sign up */}
-            <div className={styles.right}>
-                <div className={styles.login}>Log in</div>
-                |
-                <div className={styles.signup}>Sign Up</div>
-            </div>
         </div>
     </div>
 );
