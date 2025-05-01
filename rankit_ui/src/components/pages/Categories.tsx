59cf08f397f143d08d510e656961a8df26d5325c
William Cahill <drewlikesramen@gmail.com>
2025-05-01 13:33:37 -0400

    Added error handling

--- rankit_ui/src/components/pages/Categories.tsx
+++ rankit_ui/src/components/pages/Categories.tsx
@@ -1,17 +1,75 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Category, fetchCategories } from "../../utils/category";
 import { Page } from "../../utils/page";
 import "./Categories.css";
-import { SearchInput } from "../widgets/SearchInput";
 
+type State = 'NOT_LOADING' | 'LOADING' | 'ERROR';
 
 export function Categories() {
+
   const [categories, setCategories] = useState<Page<Category>>({ data: [] });
-  const [search, setSearch] = useState("");
-  const searchCategories = async (search: string) => {
-    setCategories(await fetchCategories(search));
+  const [search, setSearch] = useState('');
+  const [currentSearch, setCurrentSearch] = useState('');
+  const [state, setState] = useState<State>('NOT_LOADING');
+
+  const startSearch = async () => {
+    if(state === 'LOADING') { return };
+    setState('LOADING');
+    setCategories({ data: [] });
+    setCurrentSearch(search);
+    try {
+      const page = await fetchCategories(search);
+      setCategories(page);
+      setState('NOT_LOADING');
+    }
+    catch {
+      setState('ERROR');
+    }
   };
-  const categoryCards = categories.data.map(category => {
+
+  const continueSearch = async () => {
+    if(state === 'LOADING') { return };
+    setState('LOADING');
+    try {
+      const page = await fetchCategories(currentSearch, categories.cursor);
+      const nextCategories = {
+        data: [...categories.data, ...page.data],
+        cursor: page.cursor,
+      };
+      setCategories(nextCategories);
+      setState('NOT_LOADING');
+    }
+    catch {
+      setState('ERROR');
+    }
+  };
+
+  const clearSearch = async () => {
+    if(state === 'LOADING') { return };
+    setState('LOADING');
+    setSearch('');
+    setCurrentSearch('');
+    setCategories({ data: [] });
+    try {
+      const page = await fetchCategories('');
+      setCategories(page);
+      setState('NOT_LOADING');
+    }
+    catch {
+      setState('ERROR');
+    }
+  };
+
+  const submit = (event: FormEvent) => {
+    event.preventDefault();
+    startSearch();
+  };
+
+  useEffect(() => {
+    startSearch(); 
+  }, []);
+
+  const cards = categories.data.map(category => {
     const src = `images/categories/${category.image}`;
     return (
       <li key={category.id}>
@@ -22,13 +80,37 @@ export function Categories() {
       </li>
     )
   });
+
   return (
     <div className="Categories">
-      <SearchInput search={searchCategories} placeholder="Search Categories" />
-      <ul className="card-list">{categoryCards}</ul>
+      <form onSubmit={submit}>
+        <div className="search-wrapper">
+          <input
+            className="search"
+            value={search}
+            placeholder="Search Categories"
+            onChange={ (event: ChangeEvent<HTMLInputElement>) => setSearch(event.target.value) }
+          />
+          <button type="button" className="cross" onClick={clearSearch}>
+            <img src="images/icons/cross.svg"/>
+          </button>
+        </div>
+      </form>
+      <ul className="card-list">{cards}</ul>
+      { state === 'LOADING' && <img src="images/icons/loading.svg" className="spinner"/> }
       {
+        state === 'NOT_LOADING' &&
         categories.cursor &&
-        <button className="primary">Load More</button>
+        <button className="primary" onClick={continueSearch}>Load More</button>
+      }
+      {
+        state === 'NOT_LOADING' &&
+        categories.data.length == 0 &&
+        <span>No categories found</span>
+      }
+      {
+        state === 'ERROR' &&
+        <span className="error">An error occurred. Please try again.</span>
       }
     </div>
   )
--- rankit_ui/src/utils/category.ts
+++ rankit_ui/src/utils/category.ts
@@ -8,7 +8,7 @@ export interface Category {
 
 export async function fetchCategories(search: string, cursor?: string): Promise<Page<Category>> {
   await sleep(1000);
-  const data = allCategories.data.filter(cat => cat.name.includes(search));
+  const data = allCategories().filter(cat => cat.name.includes(search));
   const curs = cursor ? parseInt(cursor) : 0;
   const nextCursor = curs + pageSize;
   if(data.length-1 > nextCursor) {
@@ -25,135 +25,27 @@ function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const pageSize: number = 28;
 
-const pageSize = 10;
-const allCategories: Page<Category> = {
-  data: [
-    {
-      id: 1,
-      name: 'Movies',
+function allCategories(): Category[] {
+  const result: Category[] = [];
+  for(let i=1; i<=100; i+= 3) {
+    result.push({
+      id: i,
+      name: `Movies ${i}`,
       image: 'movies.svg',
-    },
-    {
-      id: 2,
-      name: 'TV Shows',
+    });
+    result.push({
+      id: i+1,
+      name: `TV Shows ${i+1}`,
       image: 'tv.svg',
-    },
-    {
-      id: 3,
-      name: 'Games',
+    });
+    result.push({
+      id: i+2,
+      name: `Games ${i+2}`,
       image: 'games.svg',
-    },
-    {
-      id: 4,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 5,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 6,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 7,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 8,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 9,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 10,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 11,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 12,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 13,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 14,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 15,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 16,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 17,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 18,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 19,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 20,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 21,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 22,
-      name: 'Movies',
-      image: 'movies.svg',
-    },
-    {
-      id: 23,
-      name: 'TV Shows',
-      image: 'tv.svg',
-    },
-    {
-      id: 24,
-      name: 'Games',
-      image: 'games.svg',
-    },
-    {
-      id: 25,
-      name: 'Games',
-      image: 'games.svg',
-    },
-  ],
-  cursor: "stub",
-};
+    });
+  }
+  return result;
+}
+
